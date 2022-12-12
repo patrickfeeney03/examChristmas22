123e03fd44904a0a10301316525a8be1032b1cce
Patrick <g00409592@atu.ie>
2022-12-12 10:01:53 +0000

    Created the constructors, setters and getters for the ShiftSupervisor.java class.

--- src/main/java/ie/atu/exam/ShiftSupervisor.java
+++ src/main/java/ie/atu/exam/ShiftSupervisor.java
@@ -1,4 +1,24 @@
 package ie.atu.exam;
 
-public class ShiftSupervisor {
+public class ShiftSupervisor extends  ShiftWorker{
+    private double annualSalary;
+
+    public ShiftSupervisor() {
+    }
+
+    public ShiftSupervisor(String name, String idNumber, int shift, int payrollNumber,
+                           double hourlyRate, double annualSalary) {
+        super(name, idNumber, shift, payrollNumber, hourlyRate);
+        this.annualSalary = annualSalary;
+    }
+
+    public double getAnnualSalary() {
+        return annualSalary;
+    }
+
+    public void setAnnualSalary(double annualSalary) {
+        this.annualSalary = annualSalary;
+    }
+
+
 }
