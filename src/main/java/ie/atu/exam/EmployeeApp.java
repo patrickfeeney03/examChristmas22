e7d3c561b29880a548cc4d1f4ebfde51982cdc08
Patrick <g00409592@atu.ie>
2022-12-12 09:57:53 +0000

    Created an object from the ShiftWorker class and used constructors to populate the private variables. Printed the contents of ShiftWorker.java by using a getReportDetails method. Forgot to mention that the class ShiftWorker.java extends the Employee.java class. !!

--- src/main/java/ie/atu/exam/EmployeeApp.java
+++ src/main/java/ie/atu/exam/EmployeeApp.java
@@ -4,10 +4,11 @@ public class EmployeeApp {
     public static void main(String[] args) {
         // getReportDetails method that displays the employee's name and the employee's id number.
         Employee employee1 = new Employee("name1", "id1");
-
+        ShiftWorker shiftworker1 = new ShiftWorker("name2", "id2", 1,
+                564, 54.66);
         //System.out.println(employee1.getName() + " " + employee1.getIdNumber());
         employee1.getReportDetails();
-
+        shiftworker1.getReportDetails();
 
 
     }
