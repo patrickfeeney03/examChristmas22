0425177f86ead5b8ebd815ed10ace7499ce93886
Patrick <g00409592@atu.ie>
2022-12-12 09:42:50 +0000

    Created EmployeeApp.java where I call Employee.java. I set the variables of an object of the Employee class inside EmployeeApp.java. I created a getReportDetails() method inside Employee.java to print the current values of the object. I have to call this method from main to print the contents.

---
+++ src/main/java/ie/atu/exam/EmployeeApp.java
@@ -0,0 +1,16 @@
+package ie.atu.exam;
+
+public class EmployeeApp {
+    public static void main(String[] args) {
+        // getReportDetails method that displays the employee's name and the employee's id number.
+        Employee employee1 = new Employee("name1", "id1ABC123");
+        //System.out.println(employee1.getName() + " " + employee1.getIdNumber());
+        employee1.getReportDetails();
+
+
+    }
+
+
+}
+
+
